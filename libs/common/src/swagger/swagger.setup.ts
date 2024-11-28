437bac1a85a5899196cf836056701cbaab650b6c
Duy Anh <duyanh.it.work@gmail.com>
2024-11-28 22:20:22 +0700

    feat: swagger

--- libs/common/src/swagger/swagger.setup.ts
+++ libs/common/src/swagger/swagger.setup.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
 	const config = new DocumentBuilder()
-		.setTitle('Freelance API Documentation')
+		.setTitle('TicketBox API Documentation')
 		.addGlobalParameters({
 			in: 'header',
 			required: false,
@@ -18,7 +18,19 @@ export function setupSwagger(app: INestApplication) {
 	const documentFactory = () => SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('docs', app, documentFactory, {
 		swaggerOptions: {
-			persistAuthorization: true
+			persistAuthorization: true,
+			tagsSorter: 'alpha',
+			operationsSorter: (a, b) => {
+				const methodsOrder = ['get', 'post', 'put', 'patch', 'delete', 'options', 'trace'];
+				let result =
+					methodsOrder.indexOf(a.get('method')) - methodsOrder.indexOf(b.get('method'));
+
+				if (result === 0) {
+					result = a.get('path').localeCompare(b.get('path'));
+				}
+
+				return result;
+			}
 		}
 	});
 }
