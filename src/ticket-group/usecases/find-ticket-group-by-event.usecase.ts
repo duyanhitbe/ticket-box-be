73984a06f274ffae1c040f5936b21ce282e7602e
duyanhitbe <duyanh.it.work@gmail.com>
2024-12-16 11:34:53 +0700

    fix: fix find ticket group by event

--- src/ticket-group/usecases/find-ticket-group-by-event.usecase.ts
+++ src/ticket-group/usecases/find-ticket-group-by-event.usecase.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {
 	FilterTicketGroupByEventDto,
+	RawTicketGroupByEventEntity,
 	TicketGroupByEventEntity,
 	TicketGroupRepository
 } from '@lib/modules/ticket-group';
@@ -24,11 +25,11 @@ export class FindTicketGroupByEventUseCase extends QueryHandler<TicketGroupByEve
 		userRoleId?: string
 	): Promise<TicketGroupByEventEntity[]> {
 		const { eventId, date } = filter;
-		const cachedData = await this.redisService.get({
-			prefix: REDIS_PREFIX_KEY.TICKET_GROUP.EVENT,
-			key: [eventId, formatDate(new Date(date), 'DD-MM-YYYY')]
-		});
-		if (cachedData) return cachedData;
+		// const cachedData = await this.redisService.get({
+		// 	prefix: REDIS_PREFIX_KEY.TICKET_GROUP.EVENT,
+		// 	key: [eventId, formatDate(new Date(date), 'DD-MM-YYYY')]
+		// });
+		// if (cachedData) return cachedData;
 
 		const customerRoleId = await this.customerRoleRepository.getCustomerRoleId(userRoleId);
 		const rawTicketGroups = await this.ticketGroupRepository.findPaginatedByEvent(
@@ -36,14 +37,10 @@ export class FindTicketGroupByEventUseCase extends QueryHandler<TicketGroupByEve
 			customerRoleId
 		);
 
-		const result = rawTicketGroups.map((item) => {
-			const res = new TicketGroupByEventEntity();
-
-			res.id = item.id;
-			res.name = item.name;
-			res.description = item.description;
-			res.ticketInfos = [];
-
+		const pushTicketInfo = (
+			res: TicketGroupByEventEntity,
+			item: RawTicketGroupByEventEntity
+		) => {
 			const haveTicketInfo = [
 				item.ticketInfoId,
 				item.ticketInfoName,
@@ -61,9 +58,26 @@ export class FindTicketGroupByEventUseCase extends QueryHandler<TicketGroupByEve
 					discountedPrice: item.ticketInfoDiscountedPrice!
 				});
 			}
+		};
 
-			return res;
-		});
+		const result = rawTicketGroups.reduce((prev: TicketGroupByEventEntity[], next) => {
+			const exist = prev.find((item) => item.id === next.id);
+
+			if (!exist) {
+				const res = new TicketGroupByEventEntity();
+
+				res.id = next.id;
+				res.name = next.name;
+				res.description = next.description;
+				res.ticketInfos = [];
+
+				pushTicketInfo(res, next);
+			} else {
+				pushTicketInfo(exist, next);
+			}
+
+			return prev;
+		}, []);
 
 		this.redisService.setNx({
 			prefix: REDIS_PREFIX_KEY.TICKET_GROUP.EVENT,
