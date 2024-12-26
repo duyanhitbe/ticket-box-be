fe50d240984e2ed47c9deb972e34d6e9c6cca477
duyanhitbe <duyanh.it.work@gmail.com>
2024-12-26 20:54:54 +0700

    feat: update migration seal customer role

--- libs/core/src/typeorm/migrations/1733036829839-seal-customer-role.ts
+++ libs/core/src/typeorm/migrations/1733036829839-seal-customer-role.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 //prettier-ignore
 export class SealCustomerRole1733036829839 implements MigrationInterface {
@@ -8,23 +8,27 @@ export class SealCustomerRole1733036829839 implements MigrationInterface {
             CREATE OR REPLACE FUNCTION prevent_normal_customer_deletion()
             RETURNS TRIGGER AS $$
             BEGIN
-                -- Prevent DELETE
-                IF TG_OP = 'DELETE' AND OLD.code = 'NORMAL_CUSTOMER' THEN
-                    RAISE EXCEPTION 'You cannot delete the NORMAL_CUSTOMER role.';
+            
+                IF TG_OP = 'DELETE' THEN
+                    IF OLD.code = 'NORMAL_CUSTOMER' THEN
+                        RAISE EXCEPTION 'Deletion of NORMAL_CUSTOMER is not allowed.';
+                    END IF;
+                    RETURN OLD;
                 END IF;
             
-                -- Prevent UPDATE on deleted_at
-                IF TG_OP = 'UPDATE' AND OLD.code = 'NORMAL_CUSTOMER' AND NEW.deleted_at IS NOT NULL THEN
-                    RAISE EXCEPTION 'You cannot delete the NORMAL_CUSTOMER role.';
+                IF TG_OP = 'UPDATE' THEN
+                    IF NEW.deleted_at IS NOT NULL AND OLD.code = 'NORMAL_CUSTOMER' THEN
+                        RAISE EXCEPTION 'Deletion of NORMAL_CUSTOMER is not allowed.';
+                    END IF;
+                    RETURN NEW;
                 END IF;
             
-                RETURN NEW;
             END;
             $$ LANGUAGE plpgsql;
         `);
         await queryRunner.query(`
             CREATE TRIGGER no_delete_normal_customer
-            BEFORE DELETE OR UPDATE ON customer_roles
+            BEFORE DELETE OR UPDATE ON public.customer_roles
             FOR EACH ROW
             EXECUTE FUNCTION prevent_normal_customer_deletion();
         `)
--- libs/modules/src/ticket-price/dto/filter-ticket-price.dto.ts
+++ libs/modules/src/ticket-price/dto/filter-ticket-price.dto.ts
@@ -1,3 +1,14 @@
 import { BaseFilterDto } from '@lib/base/dto';
+import { I18nIsUUID, Property, SwaggerProperty } from '@lib/common/decorators';
+import { IsOptional } from 'class-validator';
 
-export class FilterTicketPriceDto extends BaseFilterDto {}
+export class FilterTicketPriceDto extends BaseFilterDto {
+	/**
+	 * Mã nhóm quyền
+	 */
+	@IsOptional()
+	@I18nIsUUID()
+	@SwaggerProperty()
+	@Property('Mã nhóm quyền')
+	customerRoleId?: string;
+}
