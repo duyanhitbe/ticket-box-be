4d01c84ab5a07c2efdf2f92c07d02bf8f2f28a1d
duyanhitbe <duyanh.it.work@gmail.com>
2025-02-18 13:14:50 +0700

    feat: validate when place order

--- src/order/usecases/place-order.usecase.ts
+++ src/order/usecases/place-order.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ExecuteHandler } from '@lib/common/abstracts';
 import { ENUM_RABBITMQ_CLIENT, InjectClientRMQ, RABBITMQ_PATTERNS } from '@lib/core/rabbitmq';
 import { RabbitmqService } from '@lib/core/rabbitmq/rabbitmq.service.abstract';
@@ -9,19 +9,24 @@ import {
 	OrderRepository
 } from '@lib/modules/order';
 import { RequestUser } from '@lib/common/interfaces';
+import { CreateOrderDetailDto } from '@lib/modules/order-detail';
+import { TicketInfoRepository } from '@lib/modules/ticket-info';
 
 @Injectable()
 export class PlaceOrderUseCase extends ExecuteHandler<Promise<OrderCreatedEntity>> {
 	constructor(
 		@InjectClientRMQ(ENUM_RABBITMQ_CLIENT.ORDER)
 		private readonly orderClient: RabbitmqService,
-		private readonly orderRepository: OrderRepository
+		private readonly orderRepository: OrderRepository,
+		private readonly ticketInfoRepository: TicketInfoRepository
 	) {
 		super();
 	}
 
 	async execute(data: CreateOrderDto, user: RequestUser): Promise<OrderCreatedEntity> {
-		const { paymentMethod, note } = data;
+		const { paymentMethod, note, details } = data;
+
+		await this.validateQuantity(details);
 
 		const order = await this.orderRepository.create({
 			data: {
@@ -43,5 +48,18 @@ export class PlaceOrderUseCase extends ExecuteHandler<Promise<OrderCreatedEntity
 			id: order.id,
 			paymentMethod: order.paymentMethod
 		};
+	}
+
+	private async validateQuantity(details: CreateOrderDetailDto[]) {
+		await Promise.all(
+			details.map(async ({ ticketInfoId, quantity }) => {
+				const ticketInfo = await this.ticketInfoRepository.findByIdOrThrow({
+					id: ticketInfoId
+				});
+				if (ticketInfo.quantity < quantity) {
+					throw new BadRequestException('Số lượng vé không đủ');
+				}
+			})
+		);
 	}
 }
